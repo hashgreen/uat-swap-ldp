89b450658c69eead9e6cdc85442a5f84ede3d9be
Zilong <b01501073@g.ntu.edu.tw>
2022-12-27 22:35:51 +0800

    chore : refine component to uppercase

--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -1,4 +1,4 @@
-export { default as IconLink } from './iconLink'
-export { default as LanguageSelect } from './languageSelect'
-export { default as LearnMore } from './learnMore'
-export { default as TempBg } from './tempBg'
+export { default as IconLink } from './IconLink'
+export { default as LanguageSelect } from './LanguageSelect'
+export { default as LearnMore } from './LearnMore'
+export { default as TempBg } from './TempBg'
---
+++ src/components/landing/Hero.tsx
@@ -0,0 +1,74 @@
+import { TempBg } from '@/components/common'
+import classNames from 'classnames'
+import React from 'react'
+
+const LinkButton = ({
+    src,
+    alt,
+    href,
+    className,
+    desc,
+}: {
+    src: string
+    alt: string
+    href: string
+    className: string
+    desc: string
+}) => {
+    return (
+        <a
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={classNames(
+                'flex w-full max-w-[285px] flex-row gap-3 rounded-xl px-5 py-3 uppercase',
+                className
+            )}
+        >
+            <img src={src} alt={alt} width={24} height={24} />
+            <p className="text-lg	">{desc}</p>
+        </a>
+    )
+}
+
+const hero = () => {
+    return (
+        <div className="pyke-container-root relative z-0 h-screen">
+            {/* region start temp bg */}
+            <div className="absolute h-full w-full lg:hidden">
+                <TempBg size={160} className="-z-10" />
+            </div>
+            <div className="absolute hidden h-full w-full lg:block">
+                <TempBg size={250} className="-z-10" />
+            </div>
+            {/* region end temp bg */}
+            <div className="pyke-container flex h-full flex-col items-center justify-center text-center">
+                <p className="text-5xl md:text-6xl	">
+                    Stake, Earn, and Swap on Chia Blockchain
+                </p>
+                <p className="mt-4 text-base md:mt-10 md:text-2xl">
+                    Hashgreen brings the first decentralized liquidity staking
+                    and yield farming to you.
+                </p>
+                <div className="mt-14 flex w-full flex-wrap justify-center gap-6 xl:mt-24">
+                    <LinkButton
+                        href="https://"
+                        src="src/assets/common/discord.svg"
+                        alt="discord"
+                        className="bg-[#21B8E1]"
+                        desc="Join our community"
+                    />
+                    <LinkButton
+                        href="https://"
+                        src="src/assets/common/twitter.svg"
+                        alt="twitter"
+                        className="border"
+                        desc="Follow our Twitter"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default hero
