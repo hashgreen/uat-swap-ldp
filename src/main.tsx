93e73f896d827339a14d86670a2424ee244f6179
Zilong <b01501073@g.ntu.edu.tw>
2022-12-29 08:43:09 +0800

    feat : add react ga4

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
+import { GA_MEASUREMENT_ID } from '@/const/common'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import ReactGA from 'react-ga4'
 import App from './App'
 import './index.css'
 
+ReactGA.initialize(GA_MEASUREMENT_ID) // TODO
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
