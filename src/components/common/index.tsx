a7e9b93b0091226f1c8cacdd877d15b813e543fd
Zilong <b01501073@g.ntu.edu.tw>
2022-12-29 09:04:13 +0800

    chore : add uppercase file

---
+++ src/components/common/index.tsx
@@ -0,0 +1,4 @@
+export { default as IconLink } from './IconLink'
+export { default as LanguageSelect } from './LanguageSelect'
+export { default as LearnMore } from './LearnMore'
+export { default as TempBg } from './TempBg'
