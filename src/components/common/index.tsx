0ba7167396f01f5e97b8661dfa51bfc3fbca594d
Zilong <b01501073@g.ntu.edu.tw>
2022-12-29 08:43:54 +0800

    chore : remove lowercase files

--- src/components/common/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as IconLink } from './IconLink'
-export { default as LanguageSelect } from './LanguageSelect'
-export { default as LearnMore } from './LearnMore'
-export { default as TempBg } from './TempBg'
